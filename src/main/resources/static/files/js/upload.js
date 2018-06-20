d23a8a7e051f407de0454b774835a7a9bceaa466
chenhuanming <chenhuanming@meituan.com>
2018-06-20 20:07:49 +0800

    文件上传按钮修复

--- src/main/resources/static/files/js/upload.js
+++ src/main/resources/static/files/js/upload.js
@@ -12,6 +12,17 @@ function getUploadToken(setToken) {
     })
 }
 
+function fileChange(event) {
+    if(event.target.files[0]){
+        let filename = event.target.files[0].name
+        $("#fileButton").html(filename)
+        $("#key").val(filename)
+    }else{
+        $("#fileButton").html("请选择文件")
+    }
+
+}
+
 function uploadFile() {
 
     let form = new FormData(document.getElementById("uploadForm"))
@@ -23,7 +34,7 @@ function uploadFile() {
         contentType: false,
         success: function (result) {
             alertAutoDismiss("alert-upload", "文件上传成功")
-            setLastFile(global.domain.qiniu + result.key)
+            setLastFile(result.key,global.domain.qiniu + result.key)
         },
         error: function (error) {
             alert("上传文件失败")
@@ -31,9 +42,10 @@ function uploadFile() {
     })
 }
 
-function setLastFile(url) {
+function setLastFile(filename,url) {
     let lastFile = $("#lastUploadFile a")
     lastFile.html(url)
     lastFile.attr("href",url)
     $("#lastUploadFile").show()
+    $("#lastUploadFilename").html(filename)
 }
