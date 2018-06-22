a0892fe3d63bba9d9b3e9e08441fd32e8272509e
chenhuanming <chenhuanming@meituan.com>
2018-06-22 15:05:35 +0800

    hooks.sh

--- hooks.sh
+++ hooks.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git pull
-kill `cat pid`
+PID = `cat pid`
+(sleep 1; kill ${PID}) &
+wait ${PID}
 sbt stage
 target/universal/stage/bin/object-persistence > object-persistence.log 2>&1 &
 echo "$!" > pid
